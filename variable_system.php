385637ca68a47feae7a43e03bda6c849c5581777
acmadiqbal16 <92482236+acmadiqbal16@users.noreply.github.com>
2022-02-28 23:19:52 +0700

    Add files via upload

---
+++ variable_system.php
@@ -0,0 +1,5 @@
+<?php
+ //variable system
+ echo 'Dokumen Root' .$_SERVER ["DOKUMENT_ROOT"] ;
+ echo 'br/>Nama file ' .$_SERVER["PHP_SELF"] ;
+ ?>
---
+++ variable_user.php
@@ -0,0 +1,12 @@
+<?php
+    $nama = 'Achmad iqbal' ;
+    $umur = 21;
+    $berat = 60;
+
+    echo 'Nama : ' . $nama;
+    echo '<br/>Umur :' . $umur.' Tahun';
+    echo '<br/Berat : '.$berat. 'kg';
+
+    echo "<br/>Hello $nama Apakabar" ;
+
+    ?>
